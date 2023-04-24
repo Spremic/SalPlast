98598b73376e627e93160c40e9961211e374ec03
Djordje <dspremic1@gmail.com>
2023-04-24 21:02:21 +0200

    vercel

--- static/script/index.js
+++ static/script/index.js
@@ -1,4 +1,6 @@
 let questionContainer = document.querySelector(".contactLeft");
+let readMoreBTN = document.querySelector(".readMore");
+let textReadMore = document.querySelector(".readMoreP");
 
 questionContainer.addEventListener("click", (e) => {
   let target = e.target;
@@ -18,5 +20,17 @@ questionContainer.addEventListener("click", (e) => {
     iconX.innerHTML = "add";
     iconX.classList.remove(iconX.classList[1]);
     iconX.classList.add("add");
+  }
+});
+
+readMoreBTN.addEventListener("click", () => {
+  if (readMoreBTN.innerHTML === "Procitaj vise...") {
+    textReadMore.style.display = "block";
+    readMoreBTN.innerHTML = "Smanji";
+    readMoreBTN.style.color = "red";
+  } else if (readMoreBTN.innerHTML === "Smanji") {
+    textReadMore.style.display = "none";
+    readMoreBTN.innerHTML = "Procitaj vise...";
+    readMoreBTN.style.color = "grey";
   }
 });
